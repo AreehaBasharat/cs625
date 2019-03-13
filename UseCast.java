a51b7934b2a5a67fa74f011517b2045bcef91ef0
AreehaBasharat <47884139+AreehaBasharat@users.noreply.github.com>
2019-03-13 13:39:45 -0700

    git commit

---
+++ UseCast.java
@@ -0,0 +1,19 @@
+public class UseCast {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<5;i++){
+            System.out.println(i+"/3:"+i/3);
+        System.out.println(i+"/3 with fractions:"+(double)i/3);
+        System.out.println();
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
+}
