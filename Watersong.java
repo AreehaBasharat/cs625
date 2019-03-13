d818839b9a4a8409bf113d02c90ef2db29d57f4c
AreehaBasharat <47884139+AreehaBasharat@users.noreply.github.com>
2019-03-13 11:40:46 -0700

    git commit

---
+++ Watersong.java
@@ -0,0 +1,39 @@
+public class Watersong {
+   
+    
+    
+
+   
+    public static void main(String[] args) {
+        int waterNum=9;
+    String word="bottles";
+    while(waterNum>0)
+    {
+      
+        }
+        System.out.println(waterNum+" "+word+"of water on the wall");
+        System.out.println(waterNum+" "+word+"of water");
+        System.out.println("Take on dawn.");
+        System.out.println("pass it anround");
+        waterNum=waterNum-1;
+          if(waterNum==1)
+        {
+            word="bottle";
+        if(waterNum>0)
+        {
+            
+            System.out.println(waterNum+" "+word+"of water on the wall");
+            }
+         else
+            System.out.println("no more bottle on the wall");
+        
+            
+        
+        
+    }
+        // TODO code application logic here
+    }
+    
+}
+
+
