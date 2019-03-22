d1bacce0c3f2e4892ff972d956d59061a8aace78
AreehaBasharat <47884139+AreehaBasharat@users.noreply.github.com>
2019-03-22 12:12:15 -0700

    git commit

---
+++ JavaApplication12.java
@@ -0,0 +1,15 @@
+public class JavaApplication12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        char ch;
+        System.out.println("Press a key followes by ENTER: ");
+        ch = (char) System.in.read();
+        System.out.println("Your key is:" + ch);
+    }
+    
+    
+}
