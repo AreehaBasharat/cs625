bbb50e8e2436a646435d320a38c03baec293ef62
AreehaBasharat <47884139+AreehaBasharat@users.noreply.github.com>
2019-03-22 12:12:53 -0700

    git commit

---
+++ JavaApplication13.java
@@ -0,0 +1,19 @@
+public class JavaApplication13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        char c, answer = 'K';
+        
+        System.out.println("I'm thinking of a letter between A and Z. ");
+        System.out.println("Can you guess it:");
+        ch = (char) System.in.read();
+       
+        if (ch == answer) 
+        System.out.println("** Right **");
+    }
+    
+    
+}
